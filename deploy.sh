401422d9ddbe2cdc25ceb82825f5013193b08603
liaoqiao@corp.netease.com <liaoqiao@corp.netease.com>
2018-12-06 22:30:20 +0800

    deploy

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+
+# 确保脚本抛出遇到的错误
+set -e
+
+git add -A
+git commit -m 'deploy'
+
+# 如果发布到 https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+
+# 如果发布到 https://<USERNAME>.github.io/<REPO>
+#git push -f git@github.com:lq782655835/blogs.git master
+
+# cd -
--- node/proxy.js
+++ node/proxy.js
@@ -9,7 +9,7 @@ httpProxy.createProxyServer({target:'http://localhost:9000'}).listen(8000); // S
 // Create your target server
 //
 http.createServer(function (req, res) {
-res.writeHead(200, { 'Content-Type': 'text/plain' });
-res.write('request successfully proxied!' + '\n' + JSON.stringify(req.headers, true, 2));
-res.end();
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.write('request successfully proxied!' + '\n' + JSON.stringify(req.headers, true, 2));
+    res.end();
 }).listen(9000);
