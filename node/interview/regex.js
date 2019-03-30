976cd75d607f33865e9f847500a369f281018d82
springleo <782655835@qq.com>
2019-03-30 22:58:27 +0800

    remove ignore floder

--- node/interview/regex.js
+++ node/interview/regex.js
@@ -6,13 +6,14 @@
 // transform('1.25 发布')； // 1.25 发布
 
 function transform(str) {
-    return str.replace(/(=g)(.*?)(=)/g, (match, $1, $2, $3) => {
+    return str.replace(/(?:=g)(.*?)(=)/g, (match, $1, $2, $3) => {
+        console.log(match, $1, $2, $3)
         return '<g>' + $2 + '</g>'
     })
 }
-let str = '=g1.18 进入开发='
+let str = '=g1.18进入开发='
 let str1 = '=g1.23 联调(-1)=，=g1.25 发布(+1)='
 let str2 = '1.25 发布'
 console.log(transform(str))
-console.log(transform(str1))
-console.log(transform(str2))
+// console.log(transform(str1))
+// console.log(transform(str2))
